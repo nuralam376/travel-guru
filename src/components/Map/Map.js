810f2767f57d9d27cf383e5d962f2081d0c7a05c
Nur A Alam <nuraalam376@gmail.com>
2020-09-17 17:40:41 +0600

    added react geolocation

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,34 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import Geocode from "react-geocode";
+
+Geocode.setApiKey("AIzaSyAEeJCpUQNf3sQVCEsuu5J6Qn2C-WOovEc");
 
 const containerStyle = {
-	width: "400px",
-	height: "400px",
+	width: "100%",
+	height: "100vh",
 };
 
 const center = {
-	lat: -3.745,
-	lng: -38.523,
+	lat: 23.777176,
+	lng: 90.399452,
 };
 
 const Map = () => {
 	const [map, setMap] = useState(null);
+
+	useEffect(() => {
+		// Get latidude & longitude from address.
+		Geocode.fromAddress("Cox's bazar").then(
+			(response) => {
+				const { lat, lng } = response.results[0].geometry.location;
+				console.log("Location", lat, lng);
+			},
+			(error) => {
+				console.error("Error", error);
+			}
+		);
+	}, []);
 
 	const onLoad = useCallback(function callback(map) {
 		const bounds = new window.google.maps.LatLngBounds();
@@ -29,7 +45,7 @@ const Map = () => {
 			<GoogleMap
 				mapContainerStyle={containerStyle}
 				center={center}
-				zoom={18}
+				zoom={16}
 				onLoad={onLoad}
 				onUnmount={onUnmount}
 			></GoogleMap>
