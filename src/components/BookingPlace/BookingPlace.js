c12069664c6c5a3e0a9bcf4ba89f1a7a54a109cc
Nur A Alam <nuraalam376@gmail.com>
2020-09-17 07:41:14 +0600

    Added booking form

--- src/components/BookingPlace/BookingPlace.js
+++ src/components/BookingPlace/BookingPlace.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import places from "../../SampleData/places";
 
@@ -8,14 +9,84 @@ const BookingPlace = () => {
 	const [place, setPlace] = useState({});
 	const { name, description } = place;
 
+	const { register, handleSubmit, errors } = useForm();
+	const onSubmit = (data) => console.log(data);
+
 	useEffect(() => {
 		const placeInfo = places.find((place) => place.id === +placeId);
 		setPlace(placeInfo);
 	}, [placeId]);
 	return (
 		<Container>
-			<h1>{name}</h1>
-			<p>{description}</p>
+			<Row>
+				<Col md={4}>
+					<h1>{name}</h1>
+					<p style={{ textAlign: "justify" }}>{description}</p>
+				</Col>
+				<Col md={6} className="ml-auto">
+					<form onSubmit={handleSubmit(onSubmit)} className="w-75">
+						<label htmlFor="origin">Origin</label>
+						<input
+							name="origin"
+							ref={register({ required: true })}
+							className="form-control"
+							id="origin"
+						/>
+						{errors.origin && (
+							<span className="text-danger">* This field is required</span>
+						)}
+						<br />
+						<label htmlFor="origin">Destination</label>
+						<input
+							name="destination"
+							defaultValue={name}
+							ref={register({ required: true })}
+							className="form-control"
+							id="destination"
+							readOnly
+						/>
+						{/* errors will return when field validation fails  */}
+						{errors.destination && (
+							<span className="text-danger">* This field is required</span>
+						)}
+						<br />
+						<Row>
+							<Col md={6}>
+								<label htmlFor="from">From</label>
+								<input
+									type="date"
+									name="from"
+									ref={register({ required: true })}
+									className="form-control"
+									id="from"
+								/>
+								{errors.from && (
+									<span className="text-danger">* This field is required</span>
+								)}
+							</Col>
+							<Col md={6}>
+								<label htmlFor="to">To</label>
+								<input
+									type="date"
+									name="to"
+									ref={register({ required: true })}
+									className="form-control"
+									id="to"
+								/>
+								{errors.to && (
+									<span className="text-danger">* This field is required</span>
+								)}
+							</Col>
+						</Row>
+						<br />
+						<input
+							type="submit"
+							value="Start booking"
+							className="btn btn-warning w-100"
+						/>
+					</form>
+				</Col>
+			</Row>
 		</Container>
 	);
 };
